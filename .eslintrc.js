94712bd15691a410c9e715379df225646ab893cc
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2021-12-30 22:54:27 +0100

    build: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,25 @@
+/*
+ * Created by Martin Dünkelmann on 30.12.21, 22:53
+ * Copyright (c) 2021. All rights reserved.
+ *
+ * Last modified 30.12.21, 22:53
+ */
+
 module.exports = {
   root: true,
   env: {
     browser: true,
     node: true,
   },
-  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended', 'prettier'],
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended', 'plugin:vue/recommended', 'plugin:vuetify/base', 'plugin:prettier-vue/recommended', 'prettier'],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: ['error'],
+      },
+    ],
+  },
 }
