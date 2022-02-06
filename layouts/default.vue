3198fac21395497f1cc846b572a31ce89cc68f87
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2022-02-06 16:16:19 +0100

    fix(design): schriftzug anpassen
    fix #59
    
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <!--
-  - Created by Martin Dünkelmann on 06.02.22, 13:50
+  - Created by Martin Dünkelmann on 06.02.22, 16:18
   - Copyright (c) 2022. All rights reserved.
   -
-  - Last modified 06.02.22, 13:50
+  - Last modified 06.02.22, 16:18
   -->
 
 <template>
@@ -24,13 +24,15 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar app dense flat>
+    <v-app-bar app color="secondary" flat>
       <v-app-bar-nav-icon @click.stop="miniDrawer = !miniDrawer" />
       <v-spacer />
-      <v-app-bar-title style="flex-wrap: wrap">
-        <span class="font-weight-bold text-subtitle-2 text-sm-h6 text-no-wrap" v-text="'Herzlich willkommen beim'" />
-        <br v-if="$vuetify.breakpoint.smAndDown" />
-        <span class="font-weight-bold text-subtitle-1 text-sm-h5 text-no-wrap">RTC KÖLN e.V. <span class="secondary--text" v-text="'1972'" /></span>
+      <v-app-bar-title>
+        <span class="d-inline-block" style="text-align-last: justify">
+          <span class="font-weight-bold text-sm-h5 text-subtitle-1" v-text="'Herzlich willkommen beim'" />
+          <br />
+          <span class="font-weight-bold text-sm-h4 text-h6">RTC Köln e.V. <span class="primary--text" v-text="'1972'" /></span>
+        </span>
       </v-app-bar-title>
       <v-spacer />
     </v-app-bar>
