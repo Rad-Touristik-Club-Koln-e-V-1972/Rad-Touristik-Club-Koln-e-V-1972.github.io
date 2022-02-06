cb32dc402f7237e1a6e4f995e36528f75bc2606c
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2022-02-06 16:33:42 +0100

    fix(design): rechter Logo-Streifen breiter
    optimize code
    
    fix #60
    
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,18 @@
 <!--
-  - Created by Martin Dünkelmann on 06.02.22, 16:18
+  - Created by Martin Dünkelmann on 06.02.22, 16:33
   - Copyright (c) 2022. All rights reserved.
   -
-  - Last modified 06.02.22, 16:18
+  - Last modified 06.02.22, 16:33
   -->
 
 <template>
   <v-app id="DefaultView">
-    <v-navigation-drawer app color="secondary" mini-variant mini-variant-width="30px" permanent right touchless>
+    <v-navigation-drawer app color="secondary" mini-variant mini-variant-width="40px" permanent right touchless>
       <template #img>
         <img alt="vertikales Banner" src="/banner_vertikal.svg" />
       </template>
     </v-navigation-drawer>
-    <v-navigation-drawer :mini-variant="miniDrawer" app permanent style="border-left: #ed5700 solid 10px" touchless>
+    <v-navigation-drawer :mini-variant="miniDrawer" app permanent style="border-left: var(--v-primary-base) solid 10px" touchless>
       <v-list>
         <v-list-item v-for="(item, index) in items" :key="index" :to="item.to" exact router>
           <v-list-item-action>
@@ -41,11 +41,11 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer app style="border-left: #ed5700 solid 10px; border-right: #ed5700 solid 30px">
+    <v-footer app style="border-left: var(--v-primary-base) solid 10px; border-right: var(--v-primary-base) solid 40px">
       <span v-text="'Copyright'" />
       <v-icon left right v-text="'mdi-copyright'" />
       <!-- Datum muss der Jahreszahl der erstmaligen veröffentlichung entsprechen!-->
-      <span v-text="`2021 durch Rad-Touristik-Club Köln e.V. 1972`" />
+      <span class="" v-text="`2021 durch Rad-Touristik-Club Köln e.V. 1972`" />
     </v-footer>
   </v-app>
 </template>
