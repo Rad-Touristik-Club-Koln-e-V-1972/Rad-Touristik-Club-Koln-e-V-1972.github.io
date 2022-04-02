69f8494030e9f76f449bbf02f204106576e32e97
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2022-04-02 13:19:37 +0200

    style(ts): reorder nuxt.config.ts
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,44 +1,17 @@
 /*
- * Created by Martin Dünkelmann on 06.02.22, 15:57
+ * Created by Martin Dünkelmann on 02.04.22, 13:19
  * Copyright (c) 2022. All rights reserved.
  *
- * Last modified 06.02.22, 15:57
+ * Last modified 02.04.22, 13:19
  */
 
 import { defineNuxtConfig } from '@nuxt/bridge'
 
 export default defineNuxtConfig({
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-
-  // TODO WORKAROUND FOR https://github.com/nuxt/framework/issues/886 https://github.com/nuxt/framework/pull/2652
-  ssr: true,
-  // ssr: false, // Disable Server Side rendering since the targeted webserver doesn't run Node.JS
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    titleTemplate: '%s - Rad-Touristik-Club Köln e.V. 1972',
-    title: 'Rad-Touristik-Club Köln e.V. 1972',
-    htmlAttrs: {
-      lang: 'de',
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    transpile: [/vuetify/],
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
@@ -48,8 +21,40 @@ export default defineNuxtConfig({
     '@nuxtjs/vuetify',
   ],
 
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    htmlAttrs: {
+      lang: 'de',
+    },
+    link: [{ href: '/favicon.ico', rel: 'icon', type: 'image/x-icon' }],
+    meta: [
+      { charset: 'utf-8' },
+      { content: 'width=device-width, initial-scale=1', name: 'viewport' },
+      { content: '', hid: 'description', name: 'description' },
+      { content: 'telephone=no', name: 'format-detection' },
+    ],
+    title: 'Rad-Touristik-Club Köln e.V. 1972',
+    titleTemplate: '%s - Rad-Touristik-Club Köln e.V. 1972',
+  },
+
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // TODO WORKAROUND FOR https://github.com/nuxt/framework/issues/886 https://github.com/nuxt/framework/pull/2652
+  ssr: true,
+  // ssr: false, // Disable Server Side rendering since the targeted webserver doesn't run Node.JS
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
 
   // TODO ERROR REPORTED https://github.com/nuxt-community/vuetify-module/issues/492
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
@@ -57,10 +62,5 @@ export default defineNuxtConfig({
     customVariables: ['@/assets/variables.scss'],
     optionsPath: '@/vuetify.options.ts',
     treeShake: true,
-  },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    transpile: [/vuetify/],
   },
 })
