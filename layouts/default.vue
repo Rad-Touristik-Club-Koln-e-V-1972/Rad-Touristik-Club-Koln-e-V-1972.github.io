3e7e3da0e8c1d4d5c807f71b4f655d2a2f1a8926
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2022-04-15 21:09:32 +0200

    feat: overhaul the style of the default layout
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,32 @@
 <template>
   <v-app id="DefaultView">
-    <v-navigation-drawer :mini-variant-width="rightBannerWidth" app color="primary" mini-variant permanent right touchless>
-      <template #img>
-        <img alt="vertikales Banner" src="@/assets/img/banner_vertikal.svg" />
+    <v-app-bar app class="border" color="secondary" flat>
+      <v-app-bar-nav-icon @click.stop="navDrawer = !navDrawer">
+        <v-icon v-text="icons.mdiMenu" />
+      </v-app-bar-nav-icon>
+      <v-spacer />
+      <!--TODO WORKAROUND for  https://github.com/vuetifyjs/vuetify/issues/13455. Replace v-app-bar-title with v-toolbar-title.-->
+      <v-toolbar-title>
+        <div class="d-inline-block" style="text-align-last: justify">
+          <span class="font-weight-bold text-sm-h6 text-md-h5 text-subtitle-2" v-text="'Herzlich willkommen beim'" />
+          <br />
+          <span class="font-weight-bold text-sm-h5 text-md-h4 text-subtitle-1">RTC Köln e.V. <span class="primary--text" v-text="'1972'" /></span>
+        </div>
+      </v-toolbar-title>
+      <v-spacer />
+      <template #extension>
+        <v-tabs :show-arrows="$vuetify.breakpoint.mobile" align-with-title>
+          <v-tab>Startseite</v-tab>
+          <v-tab>Über uns</v-tab>
+          <v-tab>Kontakt</v-tab>
+          <v-tab>RTF/CTF</v-tab>
+          <v-tab>Galerie</v-tab>
+          <v-tab>Touren-Tipps</v-tab>
+        </v-tabs>
       </template>
-    </v-navigation-drawer>
-    <v-navigation-drawer :mini-variant="miniNavDrawer" app permanent style="border-left: var(--v-primary-base) solid 10px" touchless>
-      <v-list>
+    </v-app-bar>
+    <v-navigation-drawer v-model="navDrawer" bottom app temporary>
+      <v-list nav dense>
         <v-list-item v-for="(item, index) in items" :key="index" :to="item.to" exact router>
           <v-list-item-action>
             <v-icon v-text="item.icon" />
@@ -17,32 +37,7 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar app color="secondary" flat>
-      <v-app-bar-nav-icon @click.stop="miniNavDrawer = !miniNavDrawer">
-        <v-icon v-text="icons.mdiMenu" />
-      </v-app-bar-nav-icon>
-      <v-spacer />
-      <!--TODO WORKAROUND for  https://github.com/vuetifyjs/vuetify/issues/13455. Replace v-app-bar-title with v-toolbar-title.-->
-      <v-toolbar-title>
-        <div class="d-inline-block" style="text-align-last: justify">
-          <span class="font-weight-bold text-sm-h5 text-subtitle-2" v-text="'Herzlich willkommen beim'" />
-          <br />
-          <span class="font-weight-bold text-sm-h4 text-subtitle-1">RTC Köln e.V. <span class="primary--text" v-text="'1972'" /></span>
-        </div>
-      </v-toolbar-title>
-      <v-spacer />
-      <template #extension>
-        <v-tabs :show-arrows="isMobileScreen" align-with-title>
-          <v-tab>Startseite</v-tab>
-          <v-tab>Über uns</v-tab>
-          <v-tab>Kontakt</v-tab>
-          <v-tab>RTF/CTF</v-tab>
-          <v-tab>Galerie</v-tab>
-          <v-tab>Touren-Tipps</v-tab>
-        </v-tabs>
-      </template>
-    </v-app-bar>
-    <v-main>
+    <v-main class="border main">
       <v-container fluid>
         <v-row>
           <v-col>
@@ -54,25 +49,24 @@
         </v-row>
       </v-container>
     </v-main>
-    <v-footer :style="`border-left: var(--v-primary-base) solid 10px; border-right: var(--v-primary-base) solid ${rightBannerWidth}`" app class="text-caption text-sm-body-1">
-      <span v-text="'Copyright'" />
-      <v-icon :small="isMobileScreen" left right v-text="icons.mdiCopyright" />
-      <!-- Datum muss der Jahreszahl der erstmaligen veröffentlichung entsprechen!-->
-      <span v-text="`2021 durch Rad-Touristik-Club Köln e.V. 1972`" />
+    <v-footer app class="border text-body-2 text-sm-body-1 text-xl-h6">
+      <div class="text-no-wrap">
+        <v-icon :small="$vuetify.breakpoint.mobile" v-text="icons.mdiCopyright" />
+        <!-- Datum muss der Jahreszahl der erstmaligen veröffentlichung entsprechen!-->
+        2021 Rad-Touristik-Club Köln e.V. 1972
+      </div>
     </v-footer>
   </v-app>
 </template>
 
 <script lang="ts">
 import { mdiCopyright, mdiHumanGreeting, mdiMenu } from '@mdi/js'
-import { Component, Vue, Watch } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
 import CSlideshow from '~/components/layouts/default/CSlideshow.vue'
 
 @Component({ components: { CSlideshow } })
 export default class DefaultView extends Vue {
   icons = { mdiCopyright, mdiMenu }
-
-  isMobileScreen = false
 
   items = [
     {
@@ -82,18 +76,26 @@ export default class DefaultView extends Vue {
     },
   ]
 
-  miniNavDrawer = false
-
-  rightBannerWidth = '30px'
-
-  @Watch('$vuetify.breakpoint.smAndDown', { immediate: true })
-  onMobileViewChanged(val: boolean) {
-    this.isMobileScreen = val
-    this.miniNavDrawer = val
-    // noinspection MagicNumberJS
-    this.rightBannerWidth = `${val ? 30 : 40}px`
-  }
+  navDrawer = false
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#DefaultView {
+  background-attachment: fixed;
+  background-color: #ed5700;
+  background-image: url('assets/img/banner_vertikal.svg');
+  background-position: top right;
+  background-repeat: no-repeat;
+  background-size: 30px;
+}
+
+.border {
+  margin-left: 10px;
+  margin-right: 30px;
+}
+
+.main {
+  background-color: white;
+}
+</style>
