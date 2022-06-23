bccd09ae638ea392fb24aeb5b0799b61ca0fd5e2
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2022-06-23 19:48:07 +0200

    feat: use database for contacts
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- pages/contact.vue
+++ pages/contact.vue
@@ -15,50 +15,15 @@
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
 import CContact from '@/components/page/CContact.vue'
-import ContactBuilder from '@/models/builder/ContactBuilder'
+import Contact from '@/models/entities/Contact'
+import DB from '@/databases/pages/contact/DB'
 
 export default defineComponent({
     name: 'ContactView',
     components: { CContact },
     setup() {
         return {
-            items: [
-                new ContactBuilder()
-                    .description('Hier könnte seine Beschreibung stehen.')
-                    .eMail('erster.vorsitzender@rtc-koeln.de')
-                    .imageSrc('https://www.rtc-koeln.de/content/Bilder/rtc-weihnachten/2021/RTC Logo 720px_withHat.png')
-                    .name('Wolfgang Dünkelmann')
-                    .position('Erster Vorsitzender')
-                    .build(),
-                new ContactBuilder()
-                    .description('Hier könnte seine Beschreibung stehen.')
-                    .eMail('erster.sportwart@rtc-koeln.de')
-                    .imageSrc('https://www.rtc-koeln.de/content/Bilder/rtc-weihnachten/2021/RTC Logo 720px_withHat.png')
-                    .name('Klaus Dünkelmann')
-                    .position('Erster Sportwart')
-                    .build(),
-                new ContactBuilder()
-                    .description('Hier könnte seine Beschreibung stehen.')
-                    .eMail('PR@rtc-koeln.de')
-                    .imageSrc('https://www.rtc-koeln.de/content/Bilder/rtc-weihnachten/2021/RTC Logo 720px_withHat.png')
-                    .name('Diana Vialon')
-                    .position('Pressewart')
-                    .build(),
-                new ContactBuilder()
-                    .description('Hier könnte seine Beschreibung stehen.')
-                    .eMail('geschaeftsfuehrung@rtc-koeln.de')
-                    .imageSrc('https://www.rtc-koeln.de/content/Bilder/rtc-weihnachten/2021/RTC Logo 720px_withHat.png')
-                    .name('Burghild Liedtke')
-                    .position('Geschäftsführung')
-                    .build(),
-                new ContactBuilder()
-                    .description('Hier könnte seine Beschreibung stehen.')
-                    .eMail('webmaster@rtc-koeln.de')
-                    .imageSrc('https://www.rtc-koeln.de/content/Bilder/rtc-weihnachten/2021/RTC Logo 720px_withHat.png')
-                    .name('Martin Dünkelmann')
-                    .position('Webmaster')
-                    .build(),
-            ],
+            items: ([] as Contact[]).concat(DB),
         }
     },
 })
