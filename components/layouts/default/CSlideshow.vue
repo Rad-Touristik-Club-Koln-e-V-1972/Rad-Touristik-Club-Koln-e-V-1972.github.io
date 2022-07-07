6c8c8e9213e098dafa5400f1cb7da0748d1897b3
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2022-07-07 20:00:40 +0200

    feat(slideshow): show only arrows if there is more than one image
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- components/layouts/default/CSlideshow.vue
+++ components/layouts/default/CSlideshow.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-carousel id="CSlideshow" cycle height="200" hide-delimiters show-arrows-on-hover>
+    <v-carousel id="CSlideshow" cycle height="200" hide-delimiters :show-arrows="items.length > 1" show-arrows-on-hover>
         <v-carousel-item v-for="(item, i) in items" :key="i" contain :src="item" />
     </v-carousel>
 </template>
