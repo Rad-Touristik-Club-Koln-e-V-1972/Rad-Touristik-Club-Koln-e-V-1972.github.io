03d633a86e5f02c0d5f51eb9f25d58a4ba6ec47b
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2022-08-02 15:59:39 +0200

    feat(gallery): add "Nachruf Klaus Napierala"
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/store/gallery/2022/Fotogalerie.ts
+++ src/store/gallery/2022/Fotogalerie.ts
@@ -4,6 +4,7 @@ import GalleryBuilder from '~/models/builder/GalleryBuilder'
 export default new GalleryBuilder()
     .dateFrom(new Date('2022-12-31'))
     .location('Köln')
+    .image(new EntryBuilder().srcUrl('https://content.rtc-koeln.de/pages/gallery/2022/Fotogalerie/Nachruf_Klaus_Napierala.jpg').previewUrl().build())
     .image(new EntryBuilder().srcUrl('https://content.rtc-koeln.de/pages/gallery/2022/Fotogalerie/2022-05_roadbike.jpeg').previewUrl().build())
     .title('Fotogalerie')
     .titleImageUrl('https://content.rtc-koeln.de/pages/gallery/2022/Fotogalerie/preview/preview.2022-05_roadbike.jpeg')
