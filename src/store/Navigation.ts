0a0cb4c299f73cc1e816bc012ab28810628c06e3
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2022-08-04 17:44:15 +0200

    refactor(navigation): remove trailing slash
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/store/Navigation.ts
+++ src/store/Navigation.ts
@@ -36,12 +36,12 @@ export const useNavigationStore = defineStore('navigation', {
                     new ChildBuilder().icon(mdiBike).title("Empfohlene RTF's").to('index').build(),
                     new ChildBuilder().icon(mdiBike).title('Touren-Tipps').to('index').build(),
                     new ChildBuilder().icon(mdiWeb).url('https://www.radsport-events.de/termine/rennrad-events').title('Empfohlene Radsport Events').build(),
-                    new ChildBuilder().icon(mdiWeb).url('https://breitensport.rad-net.de/breitensportkalender/').title('Breitensportkalender BDR').build()
+                    new ChildBuilder().icon(mdiWeb).url('https://breitensport.rad-net.de/breitensportkalender').title('Breitensportkalender BDR').build()
                 )
                 .icon(mdiBike)
                 .title('Veranstaltungen & Termine')
                 .build(),
-            new NavigationBuilder().icon(mdiWeatherPartlyRainy).url('https://www.koeln.de/wetter/regenradar/').title('Wetter live in Köln + Regenradar').build(),
+            new NavigationBuilder().icon(mdiWeatherPartlyRainy).url('https://www.koeln.de/wetter/regenradar').title('Wetter live in Köln + Regenradar').build(),
             new NavigationBuilder()
                 .children(
                     new ChildBuilder().icon(mdiInformation).title('Über uns').to('index').build(),
@@ -55,10 +55,10 @@ export const useNavigationStore = defineStore('navigation', {
                 .build(),
         ],
         socialMedia: [
-            new NavigationBuilder().color('indigo').icon(mdiFacebook).url('https://www.facebook.com/RTC.Koeln/').build(),
-            new NavigationBuilder().color('red lighten-2').icon(mdiInstagram).url('https://www.instagram.com/rtckoeln/').build(),
-            new NavigationBuilder().color('red lighten-2').icon(mdiGoogle).url('https://www.google.de/search?q=RTC+Köln+e.V.+1972/').build(),
-            new NavigationBuilder().color('red lighten-2').icon(mdiYoutube).url('https://www.youtube.com/channel/UCC5YG6J73hWEdPoMbdjGG1w/').build(),
+            new NavigationBuilder().color('indigo').icon(mdiFacebook).url('https://www.facebook.com/RTC.Koeln').build(),
+            new NavigationBuilder().color('red lighten-2').icon(mdiInstagram).url('https://www.instagram.com/rtckoeln').build(),
+            new NavigationBuilder().color('red lighten-2').icon(mdiGoogle).url('https://www.google.de/search?q=RTC+Köln+e.V.+1972').build(),
+            new NavigationBuilder().color('red lighten-2').icon(mdiYoutube).url('https://www.youtube.com/channel/UCC5YG6J73hWEdPoMbdjGG1w').build(),
         ],
     }),
 })
