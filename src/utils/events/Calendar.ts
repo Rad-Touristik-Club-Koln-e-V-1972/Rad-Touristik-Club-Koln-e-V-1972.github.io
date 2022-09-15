a94de024b56ad8e630f6bf6397c5e62685c4ddea
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2022-09-15 18:46:38 +0200

    fix(lint): add workaround for 'date-holidays'
    issue "error HolidaysTypes not found in 'date-holidays' import/named"
    
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/utils/events/Calendar.ts
+++ src/utils/events/Calendar.ts
@@ -1,4 +1,4 @@
-import Holidays, { HolidaysTypes } from 'date-holidays'
+import Holidays from 'date-holidays'
 import EventBuilder from '~/models/builder/events/calendar/EventBuilder'
 import EEvent from '~/models/enums/EEvent'
 
@@ -6,7 +6,8 @@ export default function useCalendar() {
     const holidays = new Holidays('DE', { languages: ['de'] })
 
     return {
-        getHolidays: (year: number) =>
-            holidays.getHolidays(year).map((it: HolidaysTypes.Holiday) => new EventBuilder().allDay(true).category(EEvent.Feiertag).date(it.start, it.end).name(it.name).build()),
+        getHolidays: (
+            year: number // TODO "any" IS A WORKAROUND FOR https://github.com/commenthol/date-holidays/issues/354
+        ) => holidays.getHolidays(year).map((it: any) => new EventBuilder().allDay(true).category(EEvent.Feiertag).date(it.start, it.end).name(it.name).build()),
     }
 }
