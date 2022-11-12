6e457195abaf8497918832ea85db3f8dd169c078
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2022-11-12 13:08:47 +0100

    fix: remove workaround
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/utils/events/Calendar.ts
+++ src/utils/events/Calendar.ts
@@ -1,4 +1,4 @@
-import Holidays from 'date-holidays'
+import Holidays, * as DateHolidays from 'date-holidays'
 import EventBuilder from '~/models/builder/events/calendar/EventBuilder'
 import EEvent from '~/models/enums/EEvent'
 
@@ -6,8 +6,9 @@ export default function useCalendar() {
     const holidays = new Holidays('DE', { languages: ['de'] })
 
     return {
-        getHolidays: (
-            year: number // TODO "any" IS A WORKAROUND FOR https://github.com/commenthol/date-holidays/issues/354
-        ) => holidays.getHolidays(year).map((it: any) => new EventBuilder().allDay(true).category(EEvent.Feiertag).date(it.start, it.end).name(it.name).build()),
+        getHolidays: (year: number) =>
+            holidays
+                .getHolidays(year)
+                .map((it: DateHolidays.HolidaysTypes.Holiday) => new EventBuilder().allDay(true).category(EEvent.Feiertag).date(it.start, it.end).name(it.name).build()),
     }
 }
