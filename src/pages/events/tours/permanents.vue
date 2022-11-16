a4111c1c7431f148e0991b1a9c9bfb2a1548694e
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2022-11-16 18:12:15 +0100

    feat(event/permanente): update title
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/pages/events/tours/permanents.vue
+++ src/pages/events/tours/permanents.vue
@@ -1,7 +1,11 @@
 <template>
     <v-card id="VPermanents" flat>
-        <v-card-title class="text-body-1 text-sm-body-2 text-md-h6 text-lg-h5 text-xl-h4">Permanente</v-card-title>
-        <v-card-subtitle class="text-body-2 text-sm-subtitle-2 text-md-subtitle-1 text-lg-h6 text-xl-h5">Ganzjährig verfügbare Touren</v-card-subtitle>
+        <v-card-title class="text-body-1 text-sm-body-2 text-md-h6 text-lg-h5 text-xl-h4">Der RTC Köln e.V. bietet Euch zwei Permanente</v-card-title>
+        <v-card-subtitle class="text-body-2 text-sm-subtitle-2 text-md-subtitle-1 text-lg-h6 text-xl-h5">
+            Wir haben zwei anspruchsvolle und landschaftlich reizvolle Strecken ausgearbeitet,<br />
+            die in der Rennradsaison (auch im Winter) alleine oder in der Gruppe und<br />
+            nach eigenem Einschätzungsvermögen abgefahren werden können.
+        </v-card-subtitle>
         <v-card-text>
             <!-- TODO WORKAROUND dependency '@yeger/vue2-masonry-wall' is needed until vuetify 3.2.0 delivers native support. See https://github.com/vuetifyjs/vuetify/issues/11177 -->
             <masonry-wall :column-width="518" :gap="16" :items="useTourStore().allPermanents">
