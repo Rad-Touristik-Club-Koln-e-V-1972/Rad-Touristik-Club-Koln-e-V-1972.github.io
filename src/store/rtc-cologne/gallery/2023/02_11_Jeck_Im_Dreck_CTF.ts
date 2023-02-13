f2c2e973678140e9e5afaa2d5a3ce4a72aa75744
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2023-02-13 19:32:42 +0100

    fix(blog): date RTC GOES CTF
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/store/rtc-cologne/gallery/2023/02_11_Jeck_Im_Dreck_CTF.ts
+++ src/store/rtc-cologne/gallery/2023/02_11_Jeck_Im_Dreck_CTF.ts
@@ -2,7 +2,7 @@ import GalleryBuilder from '~/models/builder/rtc-cologne/gallery/GalleryBuilder'
 import GalleryEntryBuilder from '~/models/builder/rtc-cologne/gallery/GalleryEntryBuilder'
 
 export default new GalleryBuilder()
-    .setDate('2023-02-04')
+    .setDate('2023-02-11')
     .setId('ac7d6f63-20c4-4088-ab8e-2c7030c9fd44')
     .setImages([
         new GalleryEntryBuilder().setId('21499548-8c89-410e-b4d9-e58f9851c6f1').setImageUrl('content.rtc-koeln.de/pages/gallery/2023/02_11_Jeck_Im_Dreck_CTF/image0.jpeg').build(),
