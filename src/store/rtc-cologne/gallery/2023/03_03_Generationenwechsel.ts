0f4a06f022af4ecfc8e53f1b6385bcff5fb0fb5b
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2023-03-05 17:36:41 +0100

    feat(gallery): update der Fotos zum Vorsitzwechsel
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/store/rtc-cologne/gallery/2023/03_03_Generationenwechsel.ts
+++ src/store/rtc-cologne/gallery/2023/03_03_Generationenwechsel.ts
@@ -6,11 +6,15 @@ export default new GalleryBuilder()
     .setId('1c843630-1346-4af8-ad7b-10009b1b694e')
     .setImages([
         new GalleryEntryBuilder()
+            .setId('f12a10c2-fc4d-44ba-a0ff-e747226b3625')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2023/03_03_Generationenwechsel/IMG_6157.jpg')
+            .build(),
+        new GalleryEntryBuilder()
             .setId('4a2f521a-0dfe-4388-80e5-8658de8a0b65')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2023/03_03_Generationenwechsel/signal-2023-03-03-234808_002.jpeg')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2023/03_03_Generationenwechsel/IMG_6154.jpg')
             .build(),
     ])
     .setLocation('Köln')
     .setTitle('Generationenwechsel')
-    .setTitleImageUrl('content.rtc-koeln.de/pages/gallery/2023/03_03_Generationenwechsel/preview/preview.signal-2023-03-03-234808_002.jpeg')
+    .setTitleImageUrl('content.rtc-koeln.de/pages/gallery/2023/03_03_Generationenwechsel/preview/preview.IMG_6157.jpg')
     .build()
