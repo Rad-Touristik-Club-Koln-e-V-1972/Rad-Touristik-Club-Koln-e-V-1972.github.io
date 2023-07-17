f60f0bdfe6a11feb0dcfb2745f40aec7cadc5f08
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2023-07-17 15:59:00 +0200

    feat(news): remove Mosel-Tour
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/store/index/News.ts
+++ src/store/index/News.ts
@@ -1,16 +1,9 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
-import GalleryEntryBuilder from '~/models/builder/rtc-cologne/gallery/GalleryEntryBuilder'
 import GalleryEntry from '~/models/entities/rtc-cologne/gallery/GalleryEntry'
 
 export default defineStore('news', () => {
-    const all = ref<GalleryEntry[]>([
-        new GalleryEntryBuilder()
-            .setId('dc4f7f66-df30-405b-abd9-a0560c13e038')
-            .setImageUrl('content.rtc-koeln.de/pages/index/news/A4_Flyer_Mosel-Genussstour_RTC_Köln_2023_SMedia.pdf')
-            .setPreviewUrl('content.rtc-koeln.de/pages/index/news/preview/preview.A4_Flyer_Mosel-Genussstour_RTC_Köln_2023_SMedia.png')
-            .build(),
-    ])
+    const all = ref<GalleryEntry[]>([])
 
     return { all }
 })
