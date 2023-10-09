b3fcc29cb2e7282d79ecff1cdd12730ad1b8d9ed
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2023-10-09 18:12:16 +0200

    feat(slideshow): smaller arrows
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/components/pages/CSlideshow.vue
+++ src/components/pages/CSlideshow.vue
@@ -1,27 +1,15 @@
 <template>
-    <q-carousel
-        ref="carousel"
-        v-model="slide"
-        animated
-        :autoplay="12000"
-        control-color="primary"
-        draggable="false"
-        :height="props.height"
-        :infinite="props.infinite"
-        swipeable
-        transition-next="slide-left"
-        transition-prev="slide-right"
-    >
+    <q-carousel ref="carousel" v-model="slide" animated :autoplay="12000" control-color="primary" draggable="false" :height="props.height" :infinite="props.infinite" swipeable>
         <q-carousel-slide v-for="it in props.modelValue" :key="it.id" :name="it.id">
             <q-img class="absolute-center full-height" fit="contain" :placeholder-src="it.previewUrl.toString()" :src="it.imageUrl.toString()" style="z-index: 1" />
             <q-img class="absolute-center full-height" :src="it.previewUrl.toString()" style="filter: blur(20px)" />
         </q-carousel-slide>
         <template #control>
             <q-carousel-control position="bottom-left" style="z-index: 1">
-                <q-btn color="primary" :icon="mdiArrowLeft" round size="lg" text-color="accent" @click="$refs.carousel.previous()" />
+                <q-btn color="primary" :icon="mdiArrowLeft" round size="md" text-color="accent" @click="$refs.carousel.previous()" />
             </q-carousel-control>
             <q-carousel-control position="bottom-right" style="z-index: 1">
-                <q-btn color="primary" :icon="mdiArrowRight" round size="lg" text-color="accent" @click="$refs.carousel.next()" />
+                <q-btn color="primary" :icon="mdiArrowRight" round size="md" text-color="accent" @click="$refs.carousel.next()" />
             </q-carousel-control>
         </template>
     </q-carousel>
