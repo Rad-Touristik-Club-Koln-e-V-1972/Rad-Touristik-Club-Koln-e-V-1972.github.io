a4a8622cae1fda2261ca8a7d30b7a2d52ba91629
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2023-10-31 18:01:26 +0100

    feat(gallery): add hint about avif pictures
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/pages/rtc-cologne/galleries/PIndex.vue
+++ src/pages/rtc-cologne/galleries/PIndex.vue
@@ -1,7 +1,15 @@
 <template>
     <q-card flat>
         <q-card-section class="bg-primary text-accent text-h6">Galerien</q-card-section>
-        <q-card-section class="text-subtitle1">Fotos und Videos des RTC-Köln</q-card-section>
+        <q-card-section>
+            <span class="text-subtitle1">Fotos und Videos des RTC-Köln</span>
+            <br />
+            <span class="text">
+                Falls du keine Fotos siehst, schau bitte nach, ob dein Browser unterstützt wird:
+                <br />
+                <a href="https://caniuse.com/?search=avif" target="_blank" v-text="'https://caniuse.com/?search=avif'" />
+            </span>
+        </q-card-section>
         <q-card-section>
             <div v-for="it in Object.keys(items).reverse()" :key="it">
                 <q-card-section class="text-h6">{{ it }}</q-card-section>
