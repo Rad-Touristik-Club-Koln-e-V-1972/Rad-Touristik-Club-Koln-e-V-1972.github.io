943672c550e1e0f6a3d8713d77ff10d1c56a91d8
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2023-11-14 12:13:37 +0100

    feat(Forsbachtour): convert images to avif
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/stores/events/Tour.ts
+++ src/stores/events/Tour.ts
@@ -34,7 +34,7 @@ export default defineStore('tour', () => {
             .setImages(
                 new GalleryEntryBuilder()
                     .setId('0bb4cd1f-bfd8-478d-9722-46bcba571edb')
-                    .setImageUrl('content.rtc-koeln.de/pages/events/tours/events/rtfs/2023 Flyer.jpg')
+                    .setImageUrl('content.rtc-koeln.de/pages/events/tours/events/rtfs/2023 Flyer.jpg.avif')
                     .buildGalleryEntry(),
             )
             .setLastChange('2023-05-25')
@@ -146,14 +146,14 @@ Vom Start weg fahren wir durch eine Spielstraße, die sich leider nicht sinnvoll
 Hier bitten wir die FahrerInnen um besondere Rücksichtnahme.
 Zum Einrollen geht es zunächst flach durch die Felder und den südlichsten Stadtteil von Köln, Porz-Libur.
 <figure>
-    <img alt="Start an der Schule in Zündorf" src='https://content.rtc-koeln.de/pages/events/tours/events/rtfs/Start_Ziel.jpg' style='width: 100%' />
+    <img alt="Start an der Schule in Zündorf" src='https://content.rtc-koeln.de/pages/events/tours/events/rtfs/Start_Ziel.jpg.avif' style='width: 100%' />
     <figcaption>Start an der Schule in Zündorf</figcaption>
 </figure>
 In Troisdorf-Spich kommen dann auf dem Mauspfad die ersten Hügel.
 Nachdem in Lohmar wieder Rollen angesagt ist, geht es dann das Jabachtal hinauf.
 Zur Belohnung gibt es dann in Pohlhausen die erste Kontrolle.
 <figure>
-    <img alt="Kontrolle 1 - Pohlhausen" src='https://content.rtc-koeln.de/pages/events/tours/events/rtfs/K1_Pohlhausen.jpg' style='width: 100%' />
+    <img alt="Kontrolle 1 - Pohlhausen" src='https://content.rtc-koeln.de/pages/events/tours/events/rtfs/K1_Pohlhausen.jpg.avif' style='width: 100%' />
     <figcaption>Kontrolle 1 - Pohlhausen</figcaption>
 </figure>`,
                     )
@@ -188,13 +188,13 @@ Hier geht das Tempo noch mal deutlich nach oben, denn die alte Kölner Straße a
                         `Nach einer rasanten Abfahrt folgen die Fahrer dem Wahnbachtal bis nach Herrenteich.
 Nach einem längeren Anstieg durch Sommerhausen treffen sich im westlichen Randbereich von Much alle Strecken, bis auf die 51er.
 <figure>
-    <img alt="Von Much nach Hetzenholz" src='https://content.rtc-koeln.de/pages/events/tours/events/rtfs/Much_Hetzenholz.jpg' style='width: 100%' />
+    <img alt="Von Much nach Hetzenholz" src='https://content.rtc-koeln.de/pages/events/tours/events/rtfs/Much_Hetzenholz.jpg.avif' style='width: 100%' />
     <figcaption>Von Much nach Hetzenholz</figcaption>
 </figure>
 Von Much geht es durch wenig befahrene ländliche Straßen über Hetzenholz, Nackhausen und Seelscheid nach Ingersauel, wo die nächste Herausforderung wartet:
 der Anstieg zu unserer Kontrolle auf dem Betriebshof von Fullwood Wohnblockhaus in Oberstehöhe.
 <figure>
-    <img alt="Betriebshof Fullwood" src='https://content.rtc-koeln.de/pages/events/tours/events/rtfs/Betriebshof_Fullwood.jpg' style='width: 100%' />
+    <img alt="Betriebshof Fullwood" src='https://content.rtc-koeln.de/pages/events/tours/events/rtfs/Betriebshof_Fullwood.jpg.avif' style='width: 100%' />
     <figcaption>Betriebshof Fullwood</figcaption>
 </figure>
 Von dort geht es zunächst wellig weiter, bis nach einer schönen Abfahrt das Aggertal erreicht wird.
@@ -224,13 +224,13 @@ In Brackemich geht es rechts ab und über Eischeid hinunter ins Bröltal.
 Einer Baustelle verdanken wir den folgenden Anstieg nach Winterscheid.
 Es lohnt sich unbedingt auf der Höhe den Blick zurück ins Tal zu richten!
 <figure>
-    <img alt="Wahnbachtal" src='https://content.rtc-koeln.de/pages/events/tours/events/rtfs/Wahnbachtal.jpg' style='width: 100%' />
+    <img alt="Wahnbachtal" src='https://content.rtc-koeln.de/pages/events/tours/events/rtfs/Wahnbachtal.jpg.avif' style='width: 100%' />
     <figcaption>Wahnbachtal</figcaption>
 </figure>
 Bis zur Abfahrt zur nächsten Kontrolle in Bourauel kann man sich den Hügel auf nur leicht welliger Strecke aus den Beinen fahren.
 Leider finden sich in dieser Abfahrt einige Straßenschäden, sodass besondere Vorsicht geboten ist.
 <figure>
-    <img alt="Kontrolle 2 - Eitdorf-Bourauel" src='https://content.rtc-koeln.de/pages/events/tours/events/rtfs/K2_Eitdorf_Bourauel.jpg' style='width: 100%' />
+    <img alt="Kontrolle 2 - Eitdorf-Bourauel" src='https://content.rtc-koeln.de/pages/events/tours/events/rtfs/K2_Eitdorf_Bourauel.jpg.avif' style='width: 100%' />
     <figcaption>Kontrolle 2 - Eitdorf-Bourauel</figcaption>
 </figure>
 Von der Kontrollstelle auf dem Dorfplatz in Bourauel geht es das Siegtal hinauf.
