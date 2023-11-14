f21b89a43d960e24271b1c8644eb4ecdb0535d62
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2023-11-14 15:59:35 +0100

    feat(Gallery): convert images to avif
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/stores/rtc-cologne/gallery/2014/06_19_Erkelenz.ts
+++ src/stores/rtc-cologne/gallery/2014/06_19_Erkelenz.ts
@@ -9,22 +9,22 @@ export default new GalleryBuilder()
     .setImages([
         new GalleryEntryBuilder()
             .setId('973909ec-1c69-4f53-b995-39a38a67709d')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/06_19_Erkelenz/CAM00033.jpg')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/06_19_Erkelenz/CAM00033.jpg.avif')
             .buildGalleryEntry(),
         new GalleryEntryBuilder()
             .setId('6fda0821-db85-4e7e-8cf2-75c0c10c42bf')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/06_19_Erkelenz/CAM00034.jpg')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/06_19_Erkelenz/CAM00034.jpg.avif')
             .buildGalleryEntry(),
         new GalleryEntryBuilder()
             .setId('032c0ed9-7cf9-45bf-8fa6-5d576486db79')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/06_19_Erkelenz/CAM00035.jpg')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/06_19_Erkelenz/CAM00035.jpg.avif')
             .buildGalleryEntry(),
         new GalleryEntryBuilder()
             .setId('1abbbb28-47b0-4f06-89b3-5d87886310c7')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/06_19_Erkelenz/CAM00036.jpg')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/06_19_Erkelenz/CAM00036.jpg.avif')
             .buildGalleryEntry(),
     ])
     .setLocation('Erkelenz')
     .setTitle('Erkelenz')
-    .setTitleImageUrl('content.rtc-koeln.de/pages/gallery/2014/06_19_Erkelenz/preview/preview.CAM00034.jpg')
+    .setTitleImageUrl('content.rtc-koeln.de/pages/gallery/2014/06_19_Erkelenz/CAM00034.jpg.avif')
     .buildGallery()
