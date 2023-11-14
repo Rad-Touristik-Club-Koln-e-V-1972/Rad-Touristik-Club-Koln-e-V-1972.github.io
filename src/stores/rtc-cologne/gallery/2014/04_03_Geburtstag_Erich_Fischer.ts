eba8306a137bee8b574b0e9328ad7974a6561f14
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2023-11-14 15:41:54 +0100

    feat(Gallery): convert images to avif
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/stores/rtc-cologne/gallery/2014/04_03_Geburtstag_Erich_Fischer.ts
+++ src/stores/rtc-cologne/gallery/2014/04_03_Geburtstag_Erich_Fischer.ts
@@ -7,54 +7,54 @@ export default new GalleryBuilder()
     .setImages([
         new GalleryEntryBuilder()
             .setId('5f4c12fc-8421-4433-ba37-022857baba72')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/140403b.jpg')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/140403b.jpg.avif')
             .buildGalleryEntry(),
         new GalleryEntryBuilder()
             .setId('62c03e98-27de-4f8b-97f6-e94abc318155')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4836.JPG')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4836.JPG.avif')
             .buildGalleryEntry(),
         new GalleryEntryBuilder()
             .setId('a8c2c326-5f94-4217-8ae3-1a08879f827b')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4837.JPG')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4837.JPG.avif')
             .buildGalleryEntry(),
         new GalleryEntryBuilder()
             .setId('9611661e-bab1-4580-8211-ebed6c6c94c4')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4838.JPG')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4838.JPG.avif')
             .buildGalleryEntry(),
         new GalleryEntryBuilder()
             .setId('183155d1-df1c-45b4-9f83-564bbc5a9a80')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4839.JPG')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4839.JPG.avif')
             .buildGalleryEntry(),
         new GalleryEntryBuilder()
             .setId('6f31adf4-58ae-4b19-b92c-a586d42445c6')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4841.JPG')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4841.JPG.avif')
             .buildGalleryEntry(),
         new GalleryEntryBuilder()
             .setId('18f46342-1c62-4458-bea2-48121ea7f487')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4842.JPG')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4842.JPG.avif')
             .buildGalleryEntry(),
         new GalleryEntryBuilder()
             .setId('5daefd28-ad99-434d-9d70-ab54d76ba2ec')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4843.JPG')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4843.JPG.avif')
             .buildGalleryEntry(),
         new GalleryEntryBuilder()
             .setId('5e64fcfd-53a5-4afc-8a88-18aeeb504fac')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4848.JPG')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4848.JPG.avif')
             .buildGalleryEntry(),
         new GalleryEntryBuilder()
             .setId('7d2836ef-18ac-42ce-b3f8-16994aede457')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4849.JPG')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/DSCF4849.JPG.avif')
             .buildGalleryEntry(),
         new GalleryEntryBuilder()
             .setId('f5a6ae12-5cb6-40ce-9ed2-8fc6faa2559f')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/ErichFischerMitRad.jpg')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/ErichFischerMitRad.jpg.avif')
             .buildGalleryEntry(),
         new GalleryEntryBuilder()
             .setId('68c07626-737f-4b04-8dae-74e23038ad3e')
-            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/05_15_Presseartikel_95_Geburtstag.jpg')
+            .setImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/05_15_Presseartikel_95_Geburtstag.pdf.avif')
             .buildGalleryEntry(),
     ])
     .setLocation('Erich Fischer')
     .setTitle('Geburtstag Erich Fischer')
-    .setTitleImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/preview/preview.ErichFischerMitRad.jpg')
+    .setTitleImageUrl('content.rtc-koeln.de/pages/gallery/2014/04_03_Geburtstag_Erich_Fischer/ErichFischerMitRad.jpg.avif')
     .buildGallery()
