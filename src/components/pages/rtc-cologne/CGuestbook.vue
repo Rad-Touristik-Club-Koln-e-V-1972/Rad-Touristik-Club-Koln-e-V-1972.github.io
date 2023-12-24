8c678ead230c799668cc840d27574c5298e16c05
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2023-12-24 14:12:06 +0100

    fix(guestbook): pictures too small
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/components/pages/rtc-cologne/CGuestbook.vue
+++ src/components/pages/rtc-cologne/CGuestbook.vue
@@ -11,7 +11,7 @@
             <div v-if="props.modelValue.text">
                 <span class="text-pre-wrap" v-html="props.modelValue.text" />
             </div>
-            <c-slideshow v-if="props.modelValue.imageUrls?.length" :gallery-entries="props.modelValue.imageUrls" />
+            <c-slideshow v-if="props.modelValue.imageUrls?.length" :gallery-entries="props.modelValue.imageUrls" height="200px" />
         </q-card-section>
         <q-expansion-item v-if="props.modelValue.answer" class="text-secondary" expand-separator label="Details">
             <q-card flat>
