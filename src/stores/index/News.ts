c08b239707607021e220a1374ed2ed419e9cd774
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2024-01-02 00:31:12 +0100

    feat(News): add a new year entry
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/stores/index/News.ts
+++ src/stores/index/News.ts
@@ -8,6 +8,15 @@ export default defineStore('news', () => {
     return {
         all: ref<NewsEntry[]>([
             new NewsEntryBuilder()
+                .setId('80158519-d522-4f15-88c8-0fa61763f9f3')
+                .setImage(
+                    new GalleryEntryBuilder()
+                        .setId('b9b309af-a380-4d26-a730-32066e20d2b8')
+                        .setImageUrl('content.rtc-koeln.de/pages/index/news/signal-2024-01-01-201656.jpeg.avif')
+                        .buildGalleryEntry(),
+                )
+                .buildNewsEntry(),
+            new NewsEntryBuilder()
                 .setId('51f3e784-346c-43e2-ad72-715503ccf5ab')
                 .setImage(
                     new GalleryEntryBuilder()
