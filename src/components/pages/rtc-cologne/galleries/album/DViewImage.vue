886b3f503af0416fa951b21377ae9066bc02a67e
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2024-03-03 21:24:43 +0100

    feat(Contact): update content
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/components/pages/rtc-cologne/galleries/album/DViewImage.vue
+++ src/components/pages/rtc-cologne/galleries/album/DViewImage.vue
@@ -1,12 +1,14 @@
 <template>
-    <q-img :src="props.startValue.previewUrl.toString()" style="cursor: pointer" @click="dialog = true" />
-    <q-dialog v-model="dialog" class="z-top" maximized persistent>
-        <q-card flat>
-            <d-view-image-original :model-value="currentGalleryEntry.imageUrl" />
-            <q-btn class="absolute-top-right z-max" color="primary" :icon="mdiClose" text-color="accent" @click="dialog = false" />
-            <c-slideshow v-model="currentGalleryEntry.id" :gallery-entries="props.modelValue" :infinite="false" />
-        </q-card>
-    </q-dialog>
+    <div style="max-width: 20em">
+        <q-img :src="currentGalleryEntry.previewUrl.toString()" style="cursor: pointer" @click="dialog = true" />
+        <q-dialog v-model="dialog" class="z-top" maximized persistent>
+            <q-card flat>
+                <d-view-image-original :model-value="currentGalleryEntry.imageUrl" />
+                <q-btn class="absolute-top-right z-max" color="primary" :icon="mdiClose" text-color="accent" @click="dialog = false" />
+                <c-slideshow v-model="currentGalleryEntry.id" :gallery-entries="props.modelValue" :infinite="false" />
+            </q-card>
+        </q-dialog>
+    </div>
 </template>
 
 <script lang="ts" setup>
@@ -18,9 +20,9 @@ import GalleryEntry from 'src/models/entities/rtc-cologne/gallery/GalleryEntry'
 
 const props = defineProps<{
     modelValue: GalleryEntry[]
-    startValue: GalleryEntry
+    startValue?: GalleryEntry
 }>()
 
-const currentGalleryEntry = ref(props.startValue)
+const currentGalleryEntry = ref(props.startValue ?? props.modelValue[0])
 const dialog = ref(false)
 </script>
