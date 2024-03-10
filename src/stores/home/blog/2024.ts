e38959f2262c8b3cd0ee33447f38cf1c7be6e391
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2024-03-10 15:06:59 +0100

    feat(Blog): add entry
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/stores/home/blog/2024.ts
+++ src/stores/home/blog/2024.ts
@@ -2,6 +2,32 @@ import BlogEntryBuilder from 'src/models/builder/home/BlogEntryBuilder'
 
 export default [
     new BlogEntryBuilder()
+        .setDate('2024-03-09')
+        .setId('9080735c-8f6d-4257-b3fa-5ebed7ddb27f')
+        .setText(
+            `17 RTCler:innen kamen zum Start zu Mistral.
+Wow! Was für ein wunderschönes Bild. 😍
+Zwei konnten gesundheitsbedingt leider nicht mitfahren, haben es sich aber nicht nehmen lassen, an diesem Morgen Teil unserer tollen Gemeinschaft zu sein.
+@Norbert und Matthias: Schön, dass ihr da ward. 🫶
+Bei bestem Wetter gingen die 640 Teilnehmer:innen der RTF von Mistral auf die Strecken.
+Unsere RTC-Gruppe fuhr gemeinsam bis zur K1. Ab da ging es verteilt auf alle drei Strecken.
+VIEL gab es Freude, glückliche Gesichter, Kilometer und Tötterei.
+WENIG gab es hingegen an der 2. Kontrolle (ein paar Salzstangen) und NICHTS 🥲 an der 3. Kontrolle.
+Gerade, wenn man auf der langen Strecke ist, ist man dann ein König, wenn man in so einer Situation einen Riegel eingepackt hat oder einen geschenkt bekommt. 😉
+Das tat dem Glücksgefühl aber keinen Abbruch.
+Durch sehr schöne Landschaften in der Eifel ging es bei milden Temperaturen mal langsam und mal sehr schnell hoch und runter.
+@Mistral: Vielen Dank für die schöne Strecke. 😊
+Im Ziel haben wir noch ein paar RTCler, die auf der 70er waren, getroffen. 👍
+Ein sehr schöner Saisonstart war das.
+Und ich bin ganz stolz auf mich, weil ich heute einen richtig guten Mood und eine gute Form auf dem Rad hatte.
+Am Ende hatten Manni und ich je 132 km auf dem Tacho.
+Am 9. März - unglaublich. 🥰
+
+Eure Diana`,
+        )
+        .setTitle('Dem alten RTC Köln auf der Spur 🧡')
+        .buildBlogEntry(),
+    new BlogEntryBuilder()
         .setAlbumIDs('288bbe8e-aa03-492f-b206-8e6932e7c45d')
         .setDate('2024-03-01')
         .setId('3d9915bc-63a2-45d2-a0e6-b54de977e068')
