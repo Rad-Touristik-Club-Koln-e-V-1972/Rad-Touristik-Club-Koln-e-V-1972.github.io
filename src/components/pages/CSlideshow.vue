d889d5eb6cf6e06b884fcb7369a9a455d7a9adeb
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2024-03-12 17:46:36 +0100

    fix(CSlideshow): slide images
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/components/pages/CSlideshow.vue
+++ src/components/pages/CSlideshow.vue
@@ -1,54 +1,35 @@
 <template>
-    <q-carousel
-        ref="carousel"
-        v-model="modelValue"
-        animated
-        :autoplay="12000"
-        control-color="primary"
-        draggable="false"
-        :height="props.height"
-        :infinite="props.infinite"
-        swipeable
-    >
-        <q-carousel-slide v-for="it in props.galleryEntries" :key="it.id" :name="it.id.toString()">
+    <q-carousel ref="carousel" v-model="item" animated :autoplay="12000" control-color="primary" draggable="false" :height="props.height" :infinite="props.infinite" swipeable>
+        <q-carousel-slide v-for="it in props.galleryEntries" :key="it.id" :name="it.id">
             <q-img class="absolute-center full-height" fit="contain" :src="it.imageUrl.toString()" style="z-index: 1" />
             <q-img class="absolute-center full-height" :src="it.imageUrl.toString()" style="filter: blur(20px)" />
         </q-carousel-slide>
         <template #control>
             <q-carousel-control v-if="props.galleryEntries.length > 1" position="bottom-left" style="z-index: 1">
-                <q-btn color="primary" :icon="mdiArrowLeft" round size="md" text-color="accent" @click="previous()" />
+                <q-btn color="primary" :icon="mdiArrowLeft" round size="md" text-color="accent" @click="carousel?.previous()" />
             </q-carousel-control>
             <q-carousel-control v-if="props.galleryEntries.length > 1" position="bottom-right" style="z-index: 1">
-                <q-btn color="primary" :icon="mdiArrowRight" round size="md" text-color="accent" @click="next()" />
+                <q-btn color="primary" :icon="mdiArrowRight" round size="md" text-color="accent" @click="carousel?.next()" />
             </q-carousel-control>
         </template>
     </q-carousel>
 </template>
 
 <script lang="ts" setup>
-import { defineModel, ref } from 'vue'
+import { ref } from 'vue'
 import { QCarousel } from 'quasar'
 import { mdiArrowLeft, mdiArrowRight } from '@quasar/extras/mdi-v7'
 import GalleryEntry from 'src/models/entities/rtc-cologne/gallery/GalleryEntry'
 
-const modelValue = defineModel<string>()
-const props = withDefaults(defineProps<{ galleryEntries: GalleryEntry[]; height?: string; infinite?: boolean }>(), {
+const props = withDefaults(defineProps<{ galleryEntries: GalleryEntry[]; height?: string; infinite?: boolean; modelValue?: string }>(), {
     height: '100%',
     infinite: true,
+    modelValue: undefined,
 })
 
 const carousel = ref<InstanceType<typeof QCarousel>>()
 
-const next = () => {
-    console.warn('next: before modelValue: ', modelValue.value)
-    carousel.value?.next()
-    console.warn('next: after modelValue: ', modelValue.value)
-}
-const previous = () => {
-    carousel.value?.previous()
-}
+const item = ref(props.modelValue)
 
-console.warn('modelValue: ', modelValue.value)
-if (modelValue.value === undefined) modelValue.value = props.galleryEntries.at(0)?.id.toString()
-console.warn('modelValue: ', modelValue.value)
+if (!props.modelValue) item.value = props.galleryEntries.at(0)?.id.toString() ?? ''
 </script>
