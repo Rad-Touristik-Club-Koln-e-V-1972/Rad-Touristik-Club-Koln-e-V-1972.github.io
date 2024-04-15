2ed6909b21f5da5627b1c5b30c5c3edeee2b18b0
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2024-04-15 18:50:27 +0200

    feat: use a transition between routing
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,46 +1,52 @@
 <template>
-  <q-layout id="app" view="hHh Lpr fFf">
-    <q-header class="bg-accent border text-secondary">
-      <q-toolbar>
-        <q-btn aria-label="Menu" dense flat :icon="mdiMenu" round @click="navDrawer = !navDrawer" />
-        <q-toolbar-title class="text-center text-h5">
-          Herzlich willkommen beim
-          <br class="xs" />
-          <span class="text-bold">RTC Köln e.V. <span class="text-primary">1972</span></span>
-        </q-toolbar-title>
-        <d-accessibility />
-      </q-toolbar>
-    </q-header>
-    <c-navigation-drawer v-model="navDrawer" :class="$q.platform.is.mobile ? '' : 'border'" />
-    <q-page-container class="border">
-      <router-view />
-    </q-page-container>
-    <q-footer class="bg-accent border">
-      <div class="justify-center md-hide row sm-hide xs-hide">
-        <div v-for="it in useSponsorStore().all" :key="it.id" class="col">
-          <q-card flat>
-            <a :href="it.url?.toString()" target="_blank">
-              <q-img fit="contain" height="36px" :src="it.imageUrl?.toString()" width="100px" />
-            </a>
-          </q-card>
+  <div>
+    <q-layout id="app" view="hHh Lpr fFf">
+      <q-header class="bg-accent border text-secondary">
+        <q-toolbar>
+          <q-btn aria-label="Menu" dense flat :icon="mdiMenu" round @click="navDrawer = !navDrawer" />
+          <q-toolbar-title class="text-center text-h5">
+            Herzlich willkommen beim
+            <br class="xs" />
+            <span class="text-bold">RTC Köln e.V. <span class="text-primary">1972</span></span>
+          </q-toolbar-title>
+          <d-accessibility />
+        </q-toolbar>
+      </q-header>
+      <c-navigation-drawer v-model="navDrawer" :class="$q.platform.is.mobile ? '' : 'border'" />
+      <q-page-container class="border">
+        <router-view v-slot="{ Component, route }">
+          <transition enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
+            <component :is="Component" :key="route.path" />
+          </transition>
+        </router-view>
+      </q-page-container>
+      <q-footer class="bg-accent border">
+        <div class="justify-center md-hide row sm-hide xs-hide">
+          <div v-for="it in useSponsorStore().all" :key="it.id" class="col">
+            <q-card flat>
+              <a :href="it.url?.toString()" target="_blank">
+                <q-img fit="contain" height="36px" :src="it.imageUrl?.toString()" width="100px" />
+              </a>
+            </q-card>
+          </div>
         </div>
-      </div>
-      <div class="q-px-xs row text-body1 text-secondary">
-        <div class="col-auto">
-          <q-icon :name="mdiCopyright" size="sm" />
-          2021 - {{ new Date().getFullYear() }} RTC Köln e.V. 1972
+        <div class="q-px-xs row text-body1 text-secondary">
+          <div class="col-auto">
+            <q-icon :name="mdiCopyright" size="sm" />
+            2021 - {{ new Date().getFullYear() }} RTC Köln e.V. 1972
+          </div>
+          <div class="col" />
+          <div class="col-auto">
+            <q-btn flat :to="{ name: 'imprint' }">Impressum</q-btn>
+          </div>
+          <div class="col-auto">
+            <q-btn flat :to="{ name: 'privacy-and-security' }">Datenschutz</q-btn>
+          </div>
         </div>
-        <div class="col" />
-        <div class="col-auto">
-          <q-btn flat :to="{ name: 'imprint' }">Impressum</q-btn>
-        </div>
-        <div class="col-auto">
-          <q-btn flat :to="{ name: 'privacy-and-security' }">Datenschutz</q-btn>
-        </div>
-      </div>
-      <d-r-t-f-popup />
-    </q-footer>
-  </q-layout>
+        <d-r-t-f-popup />
+      </q-footer>
+    </q-layout>
+  </div>
 </template>
 
 <script lang="ts" setup>
