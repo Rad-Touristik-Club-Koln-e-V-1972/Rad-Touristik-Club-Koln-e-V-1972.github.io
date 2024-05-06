20d557d6560c2f795e0e6969a9a5ccb1246e5fe9
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2024-05-06 10:55:55 +0200

    feat(events/tours): optimize tracks layout
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/components/pages/events/tours/CTracks.vue
+++ src/components/pages/events/tours/CTracks.vue
@@ -4,13 +4,19 @@
       <div class="row">
         <div class="col">
           <q-timeline :layout="$q.platform.is.mobile ? 'dense' : 'loose'">
-            <q-timeline-entry v-for="(it, index) in props.tracks" :key="it.id" :side="index % 2 === 0 ? 'right' : 'left'" :title="it.name">
-              <q-card>
-                <q-card-section class="bg-primary text-accent text-h6">{{ it.name }}</q-card-section>
-                <q-card-section>
-                  <span class="text-pre-wrap" v-html="it.text" />
-                </q-card-section>
-              </q-card>
+            <q-timeline-entry v-for="(it, index) in props.tracks" :key="it.id" :side="index % 2 === 0 ? 'right' : 'left'">
+              <template #default>
+                <q-card flat>
+                  <q-card-section>
+                    <span class="text-pre-wrap" v-html="it.text" />
+                  </q-card-section>
+                </q-card>
+              </template>
+              <template #title>
+                <q-card flat>
+                  <q-card-section class="bg-primary text-accent text-h6">{{ it.name }}</q-card-section>
+                </q-card>
+              </template>
             </q-timeline-entry>
           </q-timeline>
         </div>
