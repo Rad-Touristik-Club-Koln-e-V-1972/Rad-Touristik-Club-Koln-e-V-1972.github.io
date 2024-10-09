217f8a64f77513eee39682bcefb62d8695d89dcd
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2024-10-09 17:03:37 +0200

    feat: update popup text

--- src/components/MainLayout/DRTFPopup.vue
+++ src/components/MainLayout/DRTFPopup.vue
@@ -6,38 +6,7 @@
         Unsere <b>{{ calendarStore.nextRTF.name }}</b> ist am <b>{{ useDateTime().format(calendarStore.nextRTF.start) }}.</b>
       </q-card-section>
       <q-card-section>
-        <b>Alle Strecken wurden von uns ausgeschildert.</b>
-      </q-card-section>
-      <q-card-section>
-        Es gibt 3 tolle Neuerungen:
-        <br />
-        <br />
-        ➡️ Wir bieten in diesem Jahr wieder eine 150er-Strecke an!
-        <br />
-        <br />
-        ➡️ Außerdem gibt es eine geführte Tour (82 km) für RTF-Neueinsteiger!
-        <div class="row">
-          <div class="col-1" />
-          <div class="col-auto">Abfahrt:</div>
-          <div class="col-1" />
-          <div class="col-auto">9 Uhr</div>
-        </div>
-        <div class="row">
-          <div class="col-1" />
-          <div class="col-auto">Tempo:</div>
-          <div class="col-1" />
-          <div class="col-auto">max. 20er Schnitt</div>
-        </div>
-        <div class="row">
-          <div class="col-1" />
-          <div class="col-auto"><q-icon :name="mdiArrowRightBottom" size="xs" /> Der/die langsamste Fahrer:in bestimmt die Geschwindigkeit.</div>
-        </div>
-        <br />
-        ➡️ Oder ihr nehmt unsere neue Family Tour (22 km) unter die Reifen
-        <div class="row">
-          <div class="col-1" />
-          <div class="col-auto"><q-icon :name="mdiArrowRightBottom" size="xs" />mit Fährfahrten für ein schönes Ausflugserlebnis.</div>
-        </div>
+        <b>Weitere Informationen folgen!</b>
       </q-card-section>
       <q-card-section>
         <div>Wir freuen uns auf Euch!</div>
@@ -55,7 +24,6 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { mdiArrowRightBottom } from '@quasar/extras/mdi-v7'
 import useCalendarStore from 'stores/events/Calendar'
 import useDateTime from 'src/utils/DateTime'
 
