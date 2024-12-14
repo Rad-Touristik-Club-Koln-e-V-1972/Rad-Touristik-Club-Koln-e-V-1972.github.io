9d3e860a5d6cffa134038dffd1e49950bf696128
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2024-12-14 15:17:13 +0100

    fix(gallery): wrong path of TSV Immendorf Condor Flug
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

---
+++ src/stores/rtc-cologne/gallery/2024/07_14_TSV_Immendorf_Condor_Flug.ts
@@ -0,0 +1,78 @@
+import GalleryBuilder from 'src/models/builder/rtc-cologne/gallery/GalleryBuilder'
+import GalleryEntryBuilder from 'src/models/builder/rtc-cologne/gallery/GalleryEntryBuilder'
+
+export default new GalleryBuilder()
+  .setDate('2024-07-14')
+  .setId('285ea40c-908c-4581-a073-b78a4e9adc5d')
+  .setImages([
+    new GalleryEntryBuilder()
+      .setId('6614f3ce-b550-4671-b4b0-557072ce7c19')
+      .setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/041792f3-280b-4cb5-84c4-c88914f41c7a.jpg.avif')
+      .buildGalleryEntry(),
+    new GalleryEntryBuilder()
+      .setId('55a43206-bf7d-496f-8ba4-3bf69b778a2f')
+      .setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/05b3058c-4b41-4008-9c5b-4dc869121fee.jpg.avif')
+      .buildGalleryEntry(),
+    new GalleryEntryBuilder()
+      .setId('129c905e-401f-46a3-8bdf-29f05a489f7f')
+      .setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/3b74678c-d207-46b0-a932-c8822a88f753.jpg.avif')
+      .buildGalleryEntry(),
+    new GalleryEntryBuilder()
+      .setId('a43825c1-cc44-499a-917f-6e95a80d113a')
+      .setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/9aae9501-542a-49d8-9cf1-7b48485ecb69.jpg.avif')
+      .buildGalleryEntry(),
+    new GalleryEntryBuilder()
+      .setId('a5cfda09-c01d-424a-9dec-4a7505859910')
+      .setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/dea5c588-8156-4a1e-9000-4ef958cc2ef4.jpg.avif')
+      .buildGalleryEntry(),
+    new GalleryEntryBuilder()
+      .setId('81aadc73-c11d-487b-a228-27e41b055360')
+      .setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/e1bdca27-6db5-4608-ab41-7f7c5e0b5f7b.jpg.avif')
+      .buildGalleryEntry(),
+    new GalleryEntryBuilder()
+      .setId('93f0011b-5133-47ca-9b89-56f1e0870737')
+      .setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/f634bfdf-49ed-4800-a521-90655a05c032.jpg.avif')
+      .buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('0318975d-040b-4275-a782-2009e0ee772c').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_4960.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('16f7e558-dfe7-4f10-8718-4f973b2037b0').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_4962.JPG.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('14214745-7cd2-4572-a704-d4fee7ce8b24').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_4970.JPG.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('ddf1764a-9b9a-4e0c-a667-4182fcf66e24').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_4971.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('101e6b7f-f4b5-4c80-b762-0de914769daf').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_4974.JPG.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('2d601f83-e962-420a-a6fd-356ab8777f5c').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_4975.JPG.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('b516229c-b122-4b2e-ae6b-9ad16b162f20').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_4976.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('73affc16-32e1-4f43-8231-84fb8d5d63ba').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_4983.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('40eecf13-600d-4149-93aa-ce8863e01b12').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_4984.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('ade4094e-bafe-46b9-a2f9-a23ccb9810ba').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_4986.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('558e0df5-fa2c-4f30-b612-b74ac4970b69').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_4989.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('8e08c7d6-c546-4409-b2d4-7aa9a28d9af1').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_4991.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('08eae031-9b1f-4d3d-abf9-e0fe7832c088').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_4999.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('a13f9665-fb5e-4afa-91eb-42c3e263b575').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5003.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('fb3e7695-9179-404e-a701-53f01c9518dd').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5004.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('0fb440a4-a0b9-4c9b-bf5f-aae2af16b00a').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5005.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('b2593870-e3c0-4e3f-a6fd-3f4e4e672039').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5007.JPG.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('1eaf1e97-1227-473b-a3a5-7f45af04c692').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5009.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('34b575f7-044b-4dd9-aa19-5e50252c7c46').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5011.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('a53c2db0-54c4-46d4-8c87-49c80cbf28e3').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5013.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('1a87d0c9-e0a0-4143-9714-f98a20e5f679').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5016.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('3f87f2ee-9ec3-4360-b57c-54629a13f78f').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5017.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('d170ad8a-4d3b-4924-a9d3-ce67b965a8b6').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5018.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('9bbef049-1bef-45b7-b47b-c431c3d0291e').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5021.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('9676d48d-eb2d-42b4-a926-b7bd5332e909').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5023.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('99463944-6815-40c3-890f-ff61eb9916bc').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5024.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('93ca2f37-8c72-4bf8-a78f-2886a183a1e7').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5025.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('8e13cbdd-8f16-4b9f-af2d-7aa36372eec1').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5026.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('05b60f74-9eb8-416f-b229-d1007ef24e00').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5032.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('ffa5bcaf-7f78-439b-9f1d-f4e93cb6fe55').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5035.JPG.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('3e10f3d6-9983-4385-ba71-4fc498b5c310').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5040.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('3218894f-a25a-49ac-b635-39cfeb6de066').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5042.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('756cf14c-0d14-4af7-b158-8353b641ebbf').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5051.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('cc8b16a8-d8b1-4b11-806f-6a349014aaaa').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5058.jpg.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('f8b50db6-82ae-438f-84d1-9256ea484a10').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_9165.heic.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('cb0add5a-03ed-47f0-9e67-8f108e69cf1b').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_9171.heic.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('26c74285-0c0b-4722-bd75-64f7cdceab58').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_9172.heic.avif').buildGalleryEntry(),
+    new GalleryEntryBuilder().setId('e7563b3e-e448-4292-ae81-5a85a10a1e77').setImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_9173.jpg.avif').buildGalleryEntry(),
+  ])
+  .setLocation('Köln')
+  .setTitle('30. Condor-Flug')
+  .setTitleImageUrl('content.rtc-koeln.de/pages/gallery/2024/07_14_TSV_Immendorf_Condor_Flug/IMG_5017.jpg.avif')
+  .buildGallery()
