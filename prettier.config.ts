2e9cc5f0551d4d6254117f0f940de40fb4c5afa1
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2025-02-13 19:15:30 +0100

    refactor(prettier): migrate to ts config
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

---
+++ prettier.config.ts
@@ -0,0 +1,13 @@
+export default {
+  overrides: [
+    {
+      files: 'pnpm-lock.yaml',
+      options: {
+        rangeEnd: 0,
+      },
+    },
+  ],
+  printWidth: 220,
+  semi: false,
+  singleQuote: true,
+}
