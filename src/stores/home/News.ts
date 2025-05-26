b1176764e83b2a346a78c5bb05226ca622499e93
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2025-05-26 18:47:59 +0200

    feat(News): update content
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- src/stores/home/News.ts
+++ src/stores/home/News.ts
@@ -1,27 +1,12 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import type NewsEntry from 'src/models/entities/home/NewsEntry'
-import NewsEntryBuilder from 'src/models/builder/home/NewsEntryBuilder'
-import GalleryEntryBuilder from 'src/models/builder/rtc-cologne/gallery/GalleryEntryBuilder'
 import useDateTime from 'src/utils/DateTime'
 
 export default defineStore('news', () => {
   const dateTime = useDateTime()
 
-  const all = ref<NewsEntry[]>([
-    new NewsEntryBuilder()
-      .setId('ce3e795c-7c6d-496e-83ba-fb71ff15df9f')
-      .setImage(new GalleryEntryBuilder().setId('c223b62c-1590-4fb1-b056-b62e1925551f').setImageUrl('content.rtc-koeln.de/pages/home/news/signal-2025-05-23-125023.jpeg').buildGalleryEntry())
-      .setShowUntil('2025-05-25')
-      .setText(
-        `Die FORSBACHTOUR 2025 fällt aus!
-
-Es tut uns unendlich leid, aber es soll regnerisch und stürmisch werden und eure Sicherheit liegt uns am Herzen.
-
-Euer RTC KÖLN e.V. 1972`,
-      )
-      .buildNewsEntry(),
-  ])
+  const all = ref<NewsEntry[]>([])
 
   return {
     all,
