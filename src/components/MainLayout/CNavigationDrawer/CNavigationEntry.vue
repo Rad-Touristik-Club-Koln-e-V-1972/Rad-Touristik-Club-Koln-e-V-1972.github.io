766ca828537d55268420fd18b34ad0f3969bcb89
Martin Dünkelmann <martin.duenkelmann@proton.me>
2025-09-12 19:53:41 +0200

    feat(Navigation): optimize layout
    Signed-off-by: Martin Dünkelmann <martin.duenkelmann@proton.me>

--- src/components/MainLayout/CNavigationDrawer/CNavigationEntry.vue
+++ src/components/MainLayout/CNavigationDrawer/CNavigationEntry.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <q-expansion-item v-if="props.modelValue.children?.length" expand-separator :icon="props.modelValue.icon" :label="props.modelValue.title?.toString()" switch-toggle-side>
-      <c-navigation-entry v-for="child in props.modelValue.children" :key="child.id" :model-value="child" />
+    <q-expansion-item v-if="props.modelValue.children?.length" class="q-ml-sm" expand-separator :icon="props.modelValue.icon" :label="props.modelValue.title?.toString()" switch-toggle-side>
+      <c-navigation-entry v-for="child in props.modelValue.children" :key="child.id" :is-child="true" :model-value="child" />
     </q-expansion-item>
     <q-item v-else v-ripple clickable exact :href="props.modelValue.url?.toString()" :target="props.modelValue.target" :to="props.modelValue.to">
-      <q-item-section avatar>
+      <q-item-section avatar side>
         <q-icon :name="props.modelValue.icon" />
       </q-item-section>
       <q-item-section>{{ props.modelValue.title }}</q-item-section>
