a87e91a47185b17fb853aa199f8720275d2273da
Martin Dünkelmann <martin.duenkelmann@proton.me>
2025-09-12 20:17:50 +0200

    feat(tours): add Permanente

--- src/pages/events/tours/PPermanents.vue
+++ src/pages/events/tours/PPermanents.vue
@@ -1,13 +1,13 @@
 <template>
   <q-card flat>
-    <q-card-section class="bg-primary text-accent text-h6">Der RTC Köln e.V. bietet Euch zwei Permanente</q-card-section>
+    <q-card-section class="bg-primary text-accent text-h6">Der RTC Köln e.V. bietet euch {{ permanents.length }} Permanente</q-card-section>
     <q-card-section class="text-subtitle1">
-      Wir haben zwei anspruchsvolle und landschaftlich reizvolle Strecken ausgearbeitet, die in der Rennradsaison (auch im Winter) alleine oder in der Gruppe und nach eigenem Einschätzungsvermögen abgefahren werden
-      können.
+      Wir haben {{ permanents.length }} anspruchsvolle und landschaftlich reizvolle Strecken ausgearbeitet, die in der Rennradsaison (auch im Winter) alleine oder in der Gruppe und nach eigenem Einschätzungsvermögen
+      abgefahren werden können.
     </q-card-section>
     <q-card-section>
       <div class="justify-center q-gutter-lg row">
-        <div v-for="it in useTourStore().permanents" :key="it.id" class="col-auto">
+        <div v-for="it in permanents" :key="it.id" class="col-auto">
           <c-permanent :model-value="it" />
         </div>
       </div>
@@ -18,4 +18,6 @@
 <script lang="ts" setup>
 import CPermanent from 'components/pages/events/tours/CPermanent.vue'
 import useTourStore from 'stores/events/Tour'
+
+const permanents = useTourStore().permanents
 </script>
--- src/stores/events/tours/Permanents.ts
+++ src/stores/events/tours/Permanents.ts
@@ -5,6 +5,62 @@ import EProfile from 'src/models/enums/events/EProfile'
 
 export default [
   new PermanentBuilder()
+    .setCategory(EEvent.Permanente)
+    .setControls(
+      new ControlBuilder()
+        .setCity('Rheinbach')
+        .setId('ab35dd0e-d37b-409a-b2c5-e71ae47f854f')
+        .setStreet('Bahnhofstraße 39')
+        .setTitle('Bäckerei am Rheinbacher Bahnhof')
+        .setUrl('kamps.de/standort/kamps-baeckerei-rheinbach-bahnhofstrasse-39')
+        .setZipCode('53359')
+        .buildControl(),
+      new ControlBuilder()
+        .setCity('Rheinbach-Hilberath')
+        .setId('cab9adaf-2553-4570-9f4d-17992056aed8')
+        .setStreet('Hilberather Straße 27')
+        .setTitle('Café in der alten Scheune & Hofladen Sampels')
+        .setUrl('www.scheunencafe.de')
+        .setZipCode('53359')
+        .buildControl(),
+    )
+    .setHeight(1000)
+    .setId('c9632c9a-dcdb-4d41-a621-c6a0badc1435')
+    .setLastChange('2025-09-12')
+    .setLength(130)
+    .setProfile(EProfile.Bergisch)
+    .setText(
+      `Unsere Permanente „Sahrbach Panoramarunde“! 🚴‍♀️
+
+130 km | ca. 1.000 hm | Traumhafte Eifel-Panoramen
+
+Die neue Runde des RTC Köln e.V. ist da – perfekt für alle, die Wertungskilometer sammeln und die Schönheit der Eifel genießen wollen!
+
+Highlights:
+✅ Atemberaubende Ausblicke entlang des Sahrbachs
+✅ Anspruchsvolle, aber gut fahrbare 130 km mit rund 1.000 Höhenmetern
+✅ Nach ca. 40 km bietet sich ein kleiner Stopp in der Bäckerei am Rheinbacher Bahnhof an
+✅ Kulinarischer Tipp bei km 80 – unbedingt ausprobieren: Das „Café in der alten Scheune“ in Hilberath! Bester 🍰 Kuchen 🫶 (montags geschlossen)
+
+📍 Start & Infos: https://touren.rad-net.de/vrqdvjyFWdQQ2rbMA
+
+Einfach 5 Euro auf das Konto überweisen:
+Volksbank Köln Bonn eG
+IBAN: DE73 3806 0186 66011910 14
+BIC: GENODED1BRS
+
+Und anschließend gefahrene Strecke und GC-Tourenapp Mailadresse an den Verantwortlichen zweiter.vorsitz@rtc-koeln.de schicken.
+
+Schnapp dir dein Rennrad und entdecke eine der schönsten Strecken der Region!
+
+Viel Spaß auf der Sahrbach Panoramarunde!`,
+    )
+    .setTitle('Sahrbach Panoramarunde')
+    .setUrls({
+      'Start & Infos': new URL('https://touren.rad-net.de/vrqdvjyFWdQQ2rbMA'),
+    })
+    .buildPermanent(),
+  new PermanentBuilder()
     .setActive(false)
     .setCategory(EEvent.Permanente)
     .setControls(
