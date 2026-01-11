523629c7bf8cf27b93aee71f1b5201283448e0d4
Martin Dünkelmann <martin.duenkelmann@proton.me>
2026-01-11 17:06:10 +0100

    feat: add qrcode generator component
    Signed-off-by: Martin Dünkelmann <martin.duenkelmann@proton.me>

---
+++ src/components/pages/CQRCode.vue
@@ -0,0 +1,23 @@
+<template>
+  <div ref="qrCodeRef" />
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import { useQuasar } from 'quasar'
+import QRCodeStyling from 'qr-code-styling'
+
+// noinspection LocalVariableNamingConventionJS
+const $q = useQuasar()
+
+const { url } = defineProps<{ url: URL }>()
+
+const qrCodeRef = ref()
+
+const qrCodeDimension = $q.platform.is.mobile ? 279 : 300
+const qrCode = new QRCodeStyling({ data: url.toString(), height: qrCodeDimension, type: 'svg', width: qrCodeDimension })
+
+onMounted(() => {
+  qrCode.append(qrCodeRef.value)
+})
+</script>
