969954e49d8e5aa6c33ab9a9fa0871596d028c4f
Martin Dünkelmann <martin.duenkelmann@proton.me>
2026-01-13 21:16:27 +0100

    feat: update 54. rtf content
    Signed-off-by: Martin Dünkelmann <martin.duenkelmann@proton.me>

--- src/pages/events/tours/rtfs/PFamily.vue
+++ src/pages/events/tours/rtfs/PFamily.vue
@@ -3,9 +3,9 @@
   <q-dialog :model-value="true" persistent>
     <!-- TODO Remove code until next Forsbachtour in 2026 -->
     <q-card>
-      <q-card-section class="bg-primary text-accent text-h6">Die FORSBACHTOUR 2025 fällt aus!</q-card-section>
+      <q-card-section class="bg-primary text-accent text-h6">Die FORSBACHTOUR 2026 ist am am 31. Mai 2026!</q-card-section>
       <q-card-section>
-        Es tut uns unendlich leid, aber es soll regnerisch und stürmisch werden und eure Sicherheit liegt uns am Herzen.
+        Weitere Informationen folgen!
         <br />
         <br />
         Euer RTC KÖLN e.V. 1972
