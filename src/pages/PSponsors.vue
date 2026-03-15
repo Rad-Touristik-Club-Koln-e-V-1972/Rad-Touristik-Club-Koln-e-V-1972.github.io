35b1594ea87670a8666abbf57c58d5cb7277e936
Martin Dünkelmann <martin.duenkelmann@proton.me>
2026-03-15 16:53:43 +0100

    refactor(masonry-wall): migrate from CMasonryWall component to @yeger/vue-masonry-wall dependency
    Signed-off-by: Martin Dünkelmann <martin.duenkelmann@proton.me>

--- src/pages/PSponsors.vue
+++ src/pages/PSponsors.vue
@@ -7,21 +7,26 @@
       Auch all denjenigen, die um Diskretion gebeten haben, sind wir sehr verbunden!
     </q-card-section>
     <q-card-section>
-      <c-masonry-wall v-slot="{ item }" :items="useSponsorStore().all">
-        <a :href="item.url?.toString()" target="_blank">
-          <q-card flat>
-            <q-card-section>
-              <div v-if="item.text">{{ item.text }}</div>
-              <q-img v-else-if="item.imageUrl" fit="contain" :src="item.imageUrl.toString()" />
-            </q-card-section>
-          </q-card>
-        </a>
-      </c-masonry-wall>
+      <!-- TODO Workaround until the browsers support native masonry walls. See https://caniuse.com/?search=masonry
+            & https://drafts.csswg.org/css-grid-3/
+            & https://github.com/w3c/csswg-drafts/issues?q=is%3Aopen+label%3Acss-grid-3+masonry -->
+      <masonry-wall :column-width="280" :gap="3" :items="useSponsorStore().all">
+        <template #default="{ item }">
+          <a :href="item.url?.toString()" target="_blank">
+            <q-card flat>
+              <q-card-section>
+                <div v-if="item.text">{{ item.text }}</div>
+                <q-img v-else-if="item.imageUrl" :src="item.imageUrl.toString()" fit="contain" />
+              </q-card-section>
+            </q-card>
+          </a>
+        </template>
+      </masonry-wall>
     </q-card-section>
   </q-card>
 </template>
 
 <script lang="ts" setup>
-import CMasonryWall from 'components/pages/CMasonryWall.vue'
+import { MasonryWall } from '@yeger/vue-masonry-wall'
 import useSponsorStore from 'stores/Sponsor'
 </script>
--- src/pages/events/tours/rtfs/PSuggestions.vue
+++ src/pages/events/tours/rtfs/PSuggestions.vue
@@ -9,19 +9,24 @@
       Weitere Tipps werden folgen.
     </q-card-section>
     <q-card-section>
-      <c-masonry-wall v-slot="{ item }" :items="useSuggestionStore().all">
-        <a :href="item.url.toString()" target="_blank">
-          <q-card class="bg-primary">
-            <q-card-section class="text-h6">{{ item.title }}</q-card-section>
-            <q-card-section>{{ item.text }}</q-card-section>
-          </q-card>
-        </a>
-      </c-masonry-wall>
+      <!-- TODO Workaround until the browsers support native masonry walls. See https://caniuse.com/?search=masonry
+                  & https://drafts.csswg.org/css-grid-3/
+                  & https://github.com/w3c/csswg-drafts/issues?q=is%3Aopen+label%3Acss-grid-3+masonry -->
+      <masonry-wall :column-width="280" :gap="3" :items="useSuggestionStore().all">
+        <template #default="{ item }">
+          <a :href="item.url.toString()" target="_blank">
+            <q-card class="bg-primary">
+              <q-card-section class="text-h6">{{ item.title }}</q-card-section>
+              <q-card-section>{{ item.text }}</q-card-section>
+            </q-card>
+          </a>
+        </template>
+      </masonry-wall>
     </q-card-section>
   </q-card>
 </template>
 
 <script lang="ts" setup>
-import CMasonryWall from 'components/pages/CMasonryWall.vue'
+import { MasonryWall } from '@yeger/vue-masonry-wall'
 import useSuggestionStore from 'stores/events/tours/Suggestion'
 </script>
