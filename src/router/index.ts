da8484bf3ee0ae7b14932d32584233af421fbeaf
Martin Dünkelmann <martin.duenkelmann@proton.me>
2026-06-07 12:53:02 +0200

    refactor(navigation): fix warning Nested conditional expression
    Signed-off-by: Martin Dünkelmann <martin.duenkelmann@proton.me>

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,14 +14,15 @@ import routes from './routes'
  */
 
 export default defineRouter(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER ? createMemoryHistory : process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory
+  const webHistory = process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory
+  const history = process.env.SERVER ? createMemoryHistory : webHistory
   const isNextRTFSoon = useCalendarStore().isNextRtfInDays(30)
 
   const router = createRouter({
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.VUE_ROUTER_BASE),
+    history: history(),
     routes,
     scrollBehavior: () => ({ left: 0, top: 0 }),
   })
